f3c708937f5ae16affe7fbb7f29e8d76a21b8e0e
Jeremy Knope <jeremy@astropad.com>
2022-01-19 17:14:25 -0500

    fix: reduce SQLite file size ~50%
    also run vacuum during house keeping

---
+++ migrations/2022-01-19-204521_create_metric_keys/down.sql
@@ -0,0 +1,11 @@
+DROP INDEX metrics_keys_key_idx;
+DROP TABLE metric_keys;
+DROP TABLE metrics;
+CREATE TABLE IF NOT EXISTS metrics (
+                                       id integer NOT NULL primary key autoincrement,
+                                       timestamp real NOT NULL,
+                                       key text NOT NULL,
+                                       value integer NOT NULL
+);
+CREATE INDEX IF NOT EXISTS metrics_timestamp_idx ON metrics (timestamp);
+CREATE INDEX IF NOT EXISTS metrics_key_idx ON metrics (key);
---
+++ migrations/2022-01-19-204521_create_metric_keys/up.sql
@@ -0,0 +1,16 @@
+DROP TABLE metrics;
+CREATE TABLE IF NOT EXISTS metrics (
+                                       id integer NOT NULL primary key autoincrement,
+                                       timestamp real NOT NULL,
+                                       metric_key_id integer NOT NULL,
+                                       value integer NOT NULL
+);
+CREATE INDEX IF NOT EXISTS metrics_timestamp_idx ON metrics (timestamp);
+CREATE INDEX IF NOT EXISTS metrics_key_id_idx ON metrics (metric_key_id);
+
+CREATE TABLE IF NOT EXISTS metric_keys (
+                                       id integer NOT NULL primary key autoincrement,
+                                       key text NOT NULL,
+                                       unit text
+);
+CREATE INDEX IF NOT EXISTS metrics_keys_key_idx ON metric_keys (key);
